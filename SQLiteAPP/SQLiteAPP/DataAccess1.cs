1f16a859c975f5148b673a447cf741be2b3e1fdd
NuengThanaphon01 <nueng.thanaphon01@gmail.com>
2021-04-13 21:58:21 +0700

    SQLite-HW 97

---
+++ SQLiteAPP/SQLiteAPP/DataAccess1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace SQLiteAPP
+{
+    class DataAccess1
+    {
+        public static void InitializeDatabase()
+        {
+
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Customer (uID INTEGER PRIMARY KEY, " +
+                    "first_Name NVARCHAR(30), " +
+                    "last_Name NVARCHAR(30), " +
+                    "email NVARCHAR(50))";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddData(string inputuID, string inputfirst_Name, string inputlast_Name, string inputemail)
+        {
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO Customer VALUES (@uID, @first_Name, @last_Name, @email);";
+                insertCommand.Parameters.AddWithValue("@uID", inputuID);
+                insertCommand.Parameters.AddWithValue("@first_Name", inputfirst_Name);
+                insertCommand.Parameters.AddWithValue("@last_Name", inputlast_Name);
+                insertCommand.Parameters.AddWithValue("@email", inputemail);
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
+        }
+
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db = new SqliteConnection("filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand("SELECT * from Customer", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                    entries.Add(query.GetString(1));
+                    entries.Add(query.GetString(2));
+                    entries.Add(query.GetString(3));
+                }
+
+                db.Close();
+            }
+            return entries;
+        }
+    }
+}
---
+++ SQLiteAPP/SQLiteAPP/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.Sqlite;
+
+namespace SQLiteAPP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess1.InitializeDatabase();
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            string CustomerData = "";
+
+            foreach (string data in DataAccess1.GetData())
+            {
+                CustomerData = CustomerData + data + "\n";
+            }
+            MessageBox.Show(CustomerData, "All Customer Data");
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess1.AddData(txtCustomer.Text, txtFirstname.Text, txtLastname.Text, txtMail.Text);
+            MessageBox.Show("Your data has been Added");
+            txtCustomer.Text = "";
+            txtFirstname.Text = "";
+            txtLastname.Text = "";
+            txtMail.Text = "";
+        }
+    }
+}
